839bc450293469992de700b780fdfe68b1822afa
Ima <105020194+Ima-Ekanem@users.noreply.github.com>
2022-06-21 01:15:14 +0100

    working_with_variables

---
+++ final-day-1/band_game_generator.py
@@ -0,0 +1,12 @@
+# a greeting for the program.
+print('Welcome to the Band Name Generator!')
+
+# Asks the user for the city that they grew up in.
+# "\n" goes to a new line
+city_name=input("What's name of the city you grew up in?\n")
+
+# Asks the user for the name of a pet.
+pet_name=input("What's your pet's name?\n")
+
+# Combines the name of their city and pet and show them their band name.
+print('Yor band name should be '+city_name+" "+pet_name)
---
+++ final-day-1/working_with_variables.py
@@ -0,0 +1,14 @@
+# input() will get user input in console
+# user_input is a variable that stores what the user's inputs
+
+user_input= input("What is your name?:")
+
+# Then print() will print the word "Hello" and user_input
+print("Hello " + user_input)
+
+# len() will check for length of the user_input
+#lenth_of_name is another variable that stores the length of user's input
+length_of_name=len(user_input)
+print(length_of_name)
+
+#
