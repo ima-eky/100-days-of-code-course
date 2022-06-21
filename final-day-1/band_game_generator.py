71c4380ba2ffabfe551cbb9a1d70eb1f38d6798d
Ima <105020194+Ima-Ekanem@users.noreply.github.com>
2022-06-21 02:51:39 +0100

    Update band_game_generator.py

--- final-day-1/band_game_generator.py
+++ final-day-1/band_game_generator.py
@@ -3,10 +3,10 @@ print('Welcome to the Band Name Generator!')
 
 # Asks the user for the city that they grew up in.
 # "\n" goes to a new line
-city_name=input("What's name of the city you grew up in?\n")
+city_name=input("What's name of the city you grew up in?\n").title()
 
 # Asks the user for the name of a pet.
-pet_name=input("What's your pet's name?\n")
+pet_name=input("What's your pet's name?\n").title()
 
 # Combines the name of their city and pet and show them their band name.
 print('Yor band name should be '+city_name+" "+pet_name)
