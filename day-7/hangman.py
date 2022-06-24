998e5589fcd732fdcc9ca16626e40fbc8d8de8fe
Ima <okonekanem2004@yahoo.com>
2022-06-24 23:46:44 +0100

    Added a program that ciphers text.

--- day-7/hangman.py
+++ day-7/hangman.py
@@ -6,6 +6,7 @@ from hangman_art import  stages
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
+chosen_word= "big"
 word_length = len(chosen_word)
 
 end_of_game = False
@@ -15,10 +16,12 @@ lives = 6
 display = []
 for _ in range(word_length):
     display += "_"
-
+print(f"{' '.join(display)}")
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-
+    if guess in display:
+        print(f"You have guessed this letter before")
+        continue
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
@@ -26,15 +29,13 @@ while not end_of_game:
 
     # Check if user is wrong.
     if guess not in chosen_word:
-        print(f'{guess} is not in the word,you have lost a life')
         lives -= 1
+        print(f'{guess} is not in the word,you have lost a life\nYou have {lives} left')
         if lives == 0:
             end_of_game = True
             print(f"You lose.\nTry again another time")
-
     # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
-
     # Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
@@ -42,3 +43,5 @@ while not end_of_game:
 
     # Import the stages from hangman_art.py
     print(stages[lives])
+else:
+    print(f"The correct word was {chosen_word}")
---
+++ day-8/main.py
@@ -0,0 +1,39 @@
+from art import logo
+
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char.isalpha():
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            if new_position > 25:
+                new_position = new_position % 26
+            while (new_position < 0):
+                new_position = 26 + new_position
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+should_not_end = True
+while (should_not_end):
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    should_continue = input(f"Type 'yes' if you want to continue.Otherwise type 'no':")
+    if should_continue.lower() == "no":
+        should_not_end= False
+        print(f"Goodbye")
