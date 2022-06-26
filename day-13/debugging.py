cf6bc169cd5e9bd226f25f0fba855d0d390ceec7
ima-eky <101118595+ima-eky@users.noreply.github.com>
2022-06-26 23:53:58 +0100

    Update debugging.py

--- day-13/debugging.py
+++ day-13/debugging.py
@@ -30,7 +30,7 @@ if age > 18:
 pages = 0
 word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page = int(input("Number of words per page: "))
+word_per_page == int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(f"pages = {pages}")
 print(f"word_per_page = {word_per_page}")
@@ -41,7 +41,7 @@ def mutate(a_list):
     b_list = []
     for item in a_list:
         new_item = item * 2
-        b_list.append(new_item)
+    b_list.append(new_item)
     print(b_list)
 
 mutate([1, 2, 3, 5, 8, 13])
