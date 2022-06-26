fe7b35d2c446fd2fe664fc7cd214137356c05cb4
ima-eky <101118595+ima-eky@users.noreply.github.com>
2022-06-26 22:50:09 +0100

    Update hangman.py

--- day-7/hangman.py
+++ day-7/hangman.py
@@ -6,7 +6,6 @@ from hangman_art import  stages
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
-chosen_word= "big"
 word_length = len(chosen_word)
 
 end_of_game = False
