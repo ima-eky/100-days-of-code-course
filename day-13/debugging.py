d19a80d2ed8371557b51feb7c8c87b2fbe39bf6d
ima-eky <101118595+ima-eky@users.noreply.github.com>
2022-06-26 23:49:26 +0100

    Update debugging.py

--- day-13/debugging.py
+++ day-13/debugging.py
@@ -2,7 +2,7 @@
 
 # Describe Problem
 def my_function():
-  for i in range(1, 21):
+  for i in range(1, 20):
     if i == 20:
       print("You got it")
 my_function()
@@ -10,18 +10,19 @@ my_function()
 # Reproduce the Bug
 from random import randint
 dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(0, 5)
+dice_num = randint(0, 6)
 print(dice_imgs[dice_num])
 
 # Play Computer
 year = int(input("What's your year of birth?"))
+year=1994
 if year > 1980 and year < 1994:
   print("You are a millenial.")
-elif year >= 1994:
+elif year > 1994:
   print("You are a Gen Z.")
 
 # Fix the Errors
-age = int(input("How old are you?"))
+age = input("How old are you?")
 if age > 18:
   print(f"You can drive at age {age}.")
 
